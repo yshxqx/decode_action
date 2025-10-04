83ed2eaf6fe107d360f482f31f928a0712d9931a
action <action@github.com>
2025-10-04 02:59:15 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,47 +1,82 @@
-//Wed Oct 01 2025 22:51:36 GMT+0000 (Coordinated Universal Time)
+//Sat Oct 04 2025 02:59:15 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var body = $response["\u0062\u006f\u0064\u0079"];
-let obj = JSON["\u0070\u0061\u0072\u0073\u0065"]($response["\u0062\u006f\u0064\u0079"]);
-obj = {
-  "\u0072\u0065\u0071\u0075\u0065\u0073\u0074\u005f\u0064\u0061\u0074\u0065\u005f\u006d\u0073": 0x1909d51f118,
-  "\u0072\u0065\u0071\u0075\u0065\u0073\u0074\u005f\u0064\u0061\u0074\u0065": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0034\u0035\u003a\u0031\u0031\u005a",
-  "\u0073\u0075\u0062\u0073\u0063\u0072\u0069\u0062\u0065\u0072": {
-    "\u006e\u006f\u006e\u005f\u0073\u0075\u0062\u0073\u0063\u0072\u0069\u0070\u0074\u0069\u006f\u006e\u0073": {},
-    "\u0066\u0069\u0072\u0073\u0074\u005f\u0073\u0065\u0065\u006e": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0033\u0036\u003a\u0030\u0033\u005a",
-    'original_application_version': "\u0032\u0033\u0039",
-    "\u006f\u0074\u0068\u0065\u0072\u005f\u0070\u0075\u0072\u0063\u0068\u0061\u0073\u0065\u0073": {},
-    "\u006d\u0061\u006e\u0061\u0067\u0065\u006d\u0065\u006e\u0074\u005f\u0075\u0072\u006c": 'https://apps.apple.com/account/subscriptions',
-    "\u0073\u0075\u0062\u0073\u0063\u0072\u0069\u0070\u0074\u0069\u006f\u006e\u0073": {
-      "\u0063\u006f\u006d\u002e\u0075\u007a\u0065\u0072\u006f\u002e\u0066\u0075\u006e\u0066\u006f\u0072\u0063\u0061\u006d\u002e\u0061\u006e\u0075\u0061\u006c\u0073\u0075\u0062": {
-        "\u006f\u0072\u0069\u0067\u0069\u006e\u0061\u006c\u005f\u0070\u0075\u0072\u0063\u0068\u0061\u0073\u0065\u005f\u0064\u0061\u0074\u0065": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0034\u0033\u003a\u0031\u0030\u005a",
-        "\u0065\u0078\u0070\u0069\u0072\u0065\u0073\u005f\u0064\u0061\u0074\u0065": '2999-09-09T15:43:09Z',
-        'is_sandbox': false,
-        "\u0072\u0065\u0066\u0075\u006e\u0064\u0065\u0064\u005f\u0061\u0074": null,
-        'store_transaction_id': '710001704196463',
-        "\u0075\u006e\u0073\u0075\u0062\u0073\u0063\u0072\u0069\u0062\u0065\u005f\u0064\u0065\u0074\u0065\u0063\u0074\u0065\u0064\u005f\u0061\u0074": '2024-07-10T15:44:46Z',
-        "\u0067\u0072\u0061\u0063\u0065\u005f\u0070\u0065\u0072\u0069\u006f\u0064\u005f\u0065\u0078\u0070\u0069\u0072\u0065\u0073\u005f\u0064\u0061\u0074\u0065": null,
-        "\u0070\u0065\u0072\u0069\u006f\u0064\u005f\u0074\u0079\u0070\u0065": 'trial',
-        "\u0070\u0075\u0072\u0063\u0068\u0061\u0073\u0065\u005f\u0064\u0061\u0074\u0065": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0034\u0033\u003a\u0030\u0039\u005a",
-        "\u0062\u0069\u006c\u006c\u0069\u006e\u0067\u005f\u0069\u0073\u0073\u0075\u0065\u0073\u005f\u0064\u0065\u0074\u0065\u0063\u0074\u0065\u0064\u005f\u0061\u0074": null,
-        "\u006f\u0077\u006e\u0065\u0072\u0073\u0068\u0069\u0070\u005f\u0074\u0079\u0070\u0065": "\u0050\u0055\u0052\u0043\u0048\u0041\u0053\u0045\u0044",
-        "\u0073\u0074\u006f\u0072\u0065": 'app_store',
-        "\u0061\u0075\u0074\u006f\u005f\u0072\u0065\u0073\u0075\u006d\u0065\u005f\u0064\u0061\u0074\u0065": null
+var encode_version = "jsjiami.com.v5";
+const _0x526899 = $request.url.indexOf("/addons/dplayer/") !== -1;
+const _0xf38212 = $request.url.indexOf("/vod/play/id/") !== -1;
+if (_0x526899 || _0xf38212) {
+  const _0x564d18 = Object.fromEntries(Object.entries($request.headers).map(([_0x16c23d, _0x34fdac]) => [_0x16c23d.toLowerCase(), _0x34fdac]));
+  Object.assign(_0x564d18, {
+    cookie: "group_id=3; group_name=VIP%E4%BC%9A%E5%91%98; user_check=2baffb44f3621fb3004dc81dc713df66; user_id=90106; user_name=15654564566; user_portrait=%2Fstatic%2Fimages%2Ftouxiang.png; PHPSESSID=pml7mt12bbqvr6srlp3ps8ih53; appurl=https%3A%2F%2Fapp.hxxxoo.com; load_state=0; load_time=3"
+  });
+  $done({
+    headers: _0x564d18
+  });
+} else {
+  $done({});
+}
+(function (_0x495eb7, _0x31796e, _0x507505) {
+  var _0xbaca7d = function () {
+    var _0x8baf25 = true;
+    return function (_0x22399f, _0x2bd577) {
+      var _0x21f445 = _0x8baf25 ? function () {
+        {
+          if (_0x2bd577) {
+            var _0x4be0e7 = _0x2bd577.apply(_0x22399f, arguments);
+            _0x2bd577 = null;
+            return _0x4be0e7;
+          }
+        }
+      } : function () {};
+      _0x8baf25 = false;
+      return _0x21f445;
+    };
+  }();
+  var _0x5f45d3 = _0xbaca7d(this, function () {
+    {
+      var _0x48dfad = function () {
+        {}
+      };
+      var _0x1be720 = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
+      if (!_0x1be720.console) {
+        {
+          _0x1be720.console = function (_0x537e87) {
+            var _0x507505 = {};
+            _0x507505.log = _0x537e87;
+            _0x507505.warn = _0x537e87;
+            _0x507505.debug = _0x537e87;
+            _0x507505.info = _0x537e87;
+            _0x507505.error = _0x537e87;
+            _0x507505.exception = _0x537e87;
+            _0x507505.trace = _0x537e87;
+            return _0x507505;
+          }(_0x48dfad);
+        }
+      } else {
+        _0x1be720.console.log = _0x48dfad;
+        _0x1be720.console.warn = _0x48dfad;
+        _0x1be720.console.debug = _0x48dfad;
+        _0x1be720.console.info = _0x48dfad;
+        _0x1be720.console.error = _0x48dfad;
+        _0x1be720.console.exception = _0x48dfad;
+        _0x1be720.console.trace = _0x48dfad;
       }
-    },
-    "\u0065\u006e\u0074\u0069\u0074\u006c\u0065\u006d\u0065\u006e\u0074\u0073": {
-      'ProVersion': {
-        'grace_period_expires_date': null,
-        "\u0070\u0075\u0072\u0063\u0068\u0061\u0073\u0065\u005f\u0064\u0061\u0074\u0065": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0034\u0033\u003a\u0030\u0039\u005a",
-        'product_identifier': 'com.uzero.funforcam.anualsub',
-        'expires_date': "\u0032\u0039\u0039\u0039\u002d\u0030\u0039\u002d\u0030\u0039\u0054\u0031\u0035\u003a\u0034\u0033\u003a\u0030\u0039\u005a"
+    }
+  });
+  _0x5f45d3();
+  _0x507505 = "al";
+  try {
+    {
+      _0x507505 += "ert";
+      _0x31796e = encode_version;
+      if (!(typeof _0x31796e !== "undefined" && _0x31796e === "jsjiami.com.v5")) {
+        _0x495eb7[_0x507505]("删除版本号，js会定期弹窗，还请支持我们的工作");
       }
-    },
-    "\u006f\u0072\u0069\u0067\u0069\u006e\u0061\u006c\u005f\u0070\u0075\u0072\u0063\u0068\u0061\u0073\u0065\u005f\u0064\u0061\u0074\u0065": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0033\u0035\u003a\u0032\u0035\u005a",
-    'original_app_user_id': '$RCAnonymousID:be4d127d43964399901d291adea4384e',
-    "\u006c\u0061\u0073\u0074\u005f\u0073\u0065\u0065\u006e": "\u0032\u0030\u0032\u0034\u002d\u0030\u0037\u002d\u0031\u0030\u0054\u0031\u0035\u003a\u0033\u0036\u003a\u0030\u0033\u005a"
+    }
+  } catch (_0x2d2d08) {
+    {
+      _0x495eb7[_0x507505]("删除版本号，js会定期弹窗");
+    }
   }
-};
-$done({
-  "\u0062\u006f\u0064\u0079": JSON['stringify'](obj)
-});
+})(window);
+encode_version = "jsjiami.com.v5";
